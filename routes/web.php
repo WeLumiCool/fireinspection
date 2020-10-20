f9344d7ed7916d6233a2b6c5be74ef0a46d2eeb6
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2020-10-20 11:41:02 +0600

    add libs

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+
+//Route::middleware('auth')->group(function () {
+//    Route::get('/', function () {
+//        return view('welcome', ['types' => \App\Type::all()]);
+//    })->name('main');
+//});
