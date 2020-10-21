a312d03ea9504ae786a1e36215d1dedfe742c8af
Frogy <nurs.axe@gmail.com>
2020-10-21 17:00:09 +0600

    show mobile

--- resources/views/objects/show.blade.php
+++ resources/views/objects/show.blade.php
@@ -1,16 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container py-5">
+    <?php
+        use Jenssegers\Agent\Agent;
 
-        <div class="row bg-white px-5 pb-4 mb-4 shadow" style="border-radius: 10px">
-            <div class="col-5 py-4">
+        $agent = new Agent();
+    ?>
+    <div class="container">
+
+        <div class="row bg-white px-lg-5 pb-4 mb-4 shadow" style="border-radius: 10px">
+            <div class="col-12 col-lg-5 py-4">
                 <p>
                     <span class="h3 pr-2">Имя объекта:</span> ГУМ
                 </p>
 
                 <p>
-                    <span class="h3 pr-2">Адресс:</span> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci aperiam ducimus eius, enim est explicabo ipsa laborum maxime nostrum optio provident quam quas quidem temporibus ullam veritatis voluptatibus. Harum.
+                    <span class="h3 pr-2">Адресс:</span> Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Accusantium adipisci aperiam ducimus eius, enim est explicabo ipsa laborum maxime nostrum optio
+                    provident quam quas quidem temporibus ullam veritatis voluptatibus. Harum.
                 </p>
 
                 <p>
@@ -20,19 +27,28 @@
                     <span class="h3 pr-2">Район:</span> Lorem ipsum
                 </p>
             </div>
-            <div class="col-7 mx-auto py-4">
-                <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A509077053c8294c7f409760ef365f9ab054949a326b8fb1d4b78dc03afb4f255&amp;source=constructor" width="100%" height="300" frameborder="0"></iframe>
+            <div class="col-10 col-lg-7 mx-auto py-4">
+                <iframe
+                    src="https://yandex.ru/map-widget/v1/?um=constructor%3A509077053c8294c7f409760ef365f9ab054949a326b8fb1d4b78dc03afb4f255&amp;source=constructor"
+                    width="100%" height="300" frameborder="0"></iframe>
             </div>
 
         </div>
-        <div class="row justify-content-between align-items-center">
-            <div class="col-4">
-                <h2>Инспекция</h2>
+        <div class="row justify-content-lg-between align-items-center">
+            <div class="col-6 col-lg-4">
+                <span class="h2-lg h4">Инспекция</span>
             </div>
-            <div class="col-2">
-                <button data-toggle="modal" data-target="#addStages" type="button" class="btn btn-success rounded" style="padding: 11px 19px; ">
-                    Добавить Инспекцию
-                </button>
+            <div class="col-6 col-lg-2 text-right text-lg-left">
+
+                @if($agent->isMobile())
+                    <a href="{{ route('create') }}" class="btn">
+                        <i class="fas fa-plus-circle text-success" style="font-size: 20px"></i>
+                    </a>
+                @elseif($agent->isDesktop())
+                    <a href="{{ route('create') }}" class="btn btn-success py-3 ">
+                        Добавить проверку
+                    </a>
+                @endif
             </div>
         </div>
         <div class="col-12 bg-white py-4 mt-4 shadow" style="border-radius: 10px">
@@ -40,17 +56,19 @@
                 <div class="card">
                     <div class="card-header" id="headingOne">
                         <h2 class="mb-0">
-                            <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse"
+                                    data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
                                 Collapsible Group Item #1
                                 <i class="fas fa-angle-down rotate-icon" style="margin-top: 2px;"></i>
                             </button>
                         </h2>
                     </div>
 
-                    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
+                         data-parent="#accordionExample">
                         <div class="card-body">
                             <div class="row mb-4">
-                                <div class="col-2">
+                                <div class="col-12 col-lg-2">
                                     <h6 class=font-weight-bolder>
                                         АУПС
                                     </h6>
@@ -58,7 +76,7 @@
                                         <i class="fa fa-check-circle text-success"></i>
                                     </p>
                                 </div>
-                                <div class="col-2">
+                                <div class="col-12 col-lg-2">
                                     <h6 class=font-weight-bolder>
                                         АУПТ
                                     </h6>
@@ -66,7 +84,7 @@
                                         <i class="fa fa-times-circle text-danger"></i>
                                     </p>
                                 </div>
-                                <div class="col-4">
+                                <div class="col-12 col-lg-4">
                                     <h6 class=font-weight-bolder>
                                         Первичные средства пожаротушения
                                     </h6>
@@ -78,7 +96,7 @@
                                     @endfor
 
                                 </div>
-                                <div class="col-4">
+                                <div class="col-12 col-lg-4">
                                     <h6 class=font-weight-bolder>
                                         Водоснабжение (пожарный гидрант, водоем)
                                     </h6>
@@ -89,7 +107,7 @@
                             </div>
 
                             <div class="row mb-4">
-                                <div class="col-4">
+                                <div class="col-12 col-lg-4">
                                     <h6 class=font-weight-bolder>
                                         Внутренние противопожарные краны (наличие)
                                     </h6>
@@ -97,7 +115,7 @@
                                         <i class="fa fa-check-circle text-success"></i>
                                     </p>
                                 </div>
-                                <div class="col-4">
+                                <div class="col-12 col-lg-4">
                                     <h6 class=font-weight-bolder>
                                         Планы эвакуации (наличие)
                                     </h6>
@@ -105,7 +123,7 @@
                                         <i class="fa fa-check-circle text-success"></i>
                                     </p>
                                 </div>
-                                <div class="col-4">
+                                <div class="col-12 col-lg-4">
                                     <h6 class=font-weight-bolder>
                                         Запасы пенообразования
                                     </h6>
@@ -116,11 +134,15 @@
                             </div>
 
                             <div class="row mb-4">
-                                <div class="col-9">
+                                <div class="col-12">
+                                    <p class="h4 h3-lg"></p>
+                                </div>
+                                <div class="col-12 col-lg-9">
                                     <h6 class=font-weight-bolder>
                                         Примичание
                                     </h6>
                                     @for($i = 0; $i < 3; $i++)
+                                        <img src="" alt="">
                                         <p>
                                             Lorem ipsum-{{$i}} dolor sit-{{$i}}.
                                         </p>
@@ -133,7 +155,9 @@
                 <div class="card">
                     <div class="card-header" id="headingTwo">
                         <h2 class="mb-0">
-                            <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            <button class="btn btn-link btn-block text-left collapsed" type="button"
+                                    data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false"
+                                    aria-controls="collapseTwo">
                                 Collapsible Group Item #2
                                 <i class="fas fa-angle-down rotate-icon" style="margin-top: 2px;"></i>
                             </button>
@@ -142,7 +166,7 @@
                     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
                         <div class="card-body">
                             <div class="row mb-4">
-                                <div class="col-2">
+                                <div class="col-12 col-lg-2">
                                     <h6 class=font-weight-bolder>
                                         АУПС
                                     </h6>
@@ -150,7 +174,7 @@
                                         <i class="fa fa-check-circle text-success"></i>
                                     </p>
                                 </div>
-                                <div class="col-2">
+                                <div class="col-12 col-lg-2">
                                     <h6 class=font-weight-bolder>
                                         АУПТ
                                     </h6>
@@ -158,7 +182,7 @@
                                         <i class="fa fa-times-circle text-danger"></i>
                                     </p>
                                 </div>
-                                <div class="col-4">
+                                <div class="col-12 col-lg-4">
                                     <h6 class=font-weight-bolder>
                                         Первичные средства пожаротушения
                                     </h6>
@@ -170,7 +194,7 @@
                                     @endfor
 
                                 </div>
-                                <div class="col-4">
+                                <div class="col-12 col-lg-4">
                                     <h6 class=font-weight-bolder>
                                         Водоснабжение (пожарный гидрант, водоем)
                                     </h6>
@@ -181,7 +205,7 @@
                             </div>
 
                             <div class="row mb-4">
-                                <div class="col-4">
+                                <div class="col-12 col-lg-4">
                                     <h6 class=font-weight-bolder>
                                         Внутренние противопожарные краны (наличие)
                                     </h6>
@@ -189,7 +213,7 @@
                                         <i class="fa fa-check-circle text-success"></i>
                                     </p>
                                 </div>
-                                <div class="col-4">
+                                <div class="col-12 col-lg-4">
                                     <h6 class=font-weight-bolder>
                                         Планы эвакуации (наличие)
                                     </h6>
@@ -197,7 +221,7 @@
                                         <i class="fa fa-check-circle text-success"></i>
                                     </p>
                                 </div>
-                                <div class="col-4">
+                                <div class="col-12 col-lg-4">
                                     <h6 class=font-weight-bolder>
                                         Запасы пенообразования
                                     </h6>
@@ -208,7 +232,7 @@
                             </div>
 
                             <div class="row mb-4">
-                                <div class="col-9">
+                                <div class="col-12 col-lg-9">
                                     <h6 class=font-weight-bolder>
                                         Примичание
                                     </h6>
@@ -225,16 +249,19 @@
                 <div class="card">
                     <div class="card-header" id="headingThree">
                         <h2 class="mb-0">
-                            <button class="btn btn-link btn-block text-left collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            <button class="btn btn-link btn-block text-left collapsed" type="button"
+                                    data-toggle="collapse" data-target="#collapseThree" aria-expanded="false"
+                                    aria-controls="collapseThree">
                                 Collapsible Group Item #3
                                 <i class="fas fa-angle-down rotate-icon" style="margin-top: 2px;"></i>
                             </button>
                         </h2>
                     </div>
-                    <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                    <div id="collapseThree" class="collapse" aria-labelledby="headingThree"
+                         data-parent="#accordionExample">
                         <div class="card-body">
                             <div class="row mb-4">
-                                <div class="col-2">
+                                <div class="col-12 col-lg-2">
                                     <h6 class=font-weight-bolder>
                                         АУПС
                                     </h6>
@@ -242,7 +269,7 @@
                                         <i class="fa fa-check-circle text-success"></i>
                                     </p>
                                 </div>
-                                <div class="col-2">
+                                <div class="col-12 col-lg-2">
                                     <h6 class=font-weight-bolder>
                                         АУПТ
                                     </h6>
@@ -250,7 +277,7 @@
                                         <i class="fa fa-times-circle text-danger"></i>
                                     </p>
                                 </div>
-                                <div class="col-4">
+                                <div class="col-12 col-lg-4">
                                     <h6 class=font-weight-bolder>
                                         Первичные средства пожаротушения
                                     </h6>
@@ -262,7 +289,7 @@
                                     @endfor
 
                                 </div>
-                                <div class="col-4">
+                                <div class="col-12 col-lg-4">
                                     <h6 class=font-weight-bolder>
                                         Водоснабжение (пожарный гидрант, водоем)
                                     </h6>
@@ -273,7 +300,7 @@
                             </div>
 
                             <div class="row mb-4">
-                                <div class="col-4">
+                                <div class="col-12 col-lg-4">
                                     <h6 class=font-weight-bolder>
                                         Внутренние противопожарные краны (наличие)
                                     </h6>
@@ -281,7 +308,7 @@
                                         <i class="fa fa-check-circle text-success"></i>
                                     </p>
                                 </div>
-                                <div class="col-4">
+                                <div class="col-12 col-lg-4">
                                     <h6 class=font-weight-bolder>
                                         Планы эвакуации (наличие)
                                     </h6>
@@ -289,7 +316,7 @@
                                         <i class="fa fa-check-circle text-success"></i>
                                     </p>
                                 </div>
-                                <div class="col-4">
+                                <div class="col-12 col-lg-4">
                                     <h6 class=font-weight-bolder>
                                         Запасы пенообразования
                                     </h6>
@@ -300,7 +327,7 @@
                             </div>
 
                             <div class="row mb-4">
-                                <div class="col-9">
+                                <div class="col-12 col-lg-9">
                                     <h6 class=font-weight-bolder>
                                         Примичание
                                     </h6>
@@ -317,5 +344,4 @@
             </div>
         </div>
     </div>
-    @include('models.model_add_fireinspection')
 @endsection
