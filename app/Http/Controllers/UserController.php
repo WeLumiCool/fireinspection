1f2160e06f7f7af461f2bb11b9735a42c511742b
Damir <Damir290598@gmail.com>
2020-10-22 17:50:38 +0600

    done change permission

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -124,7 +124,7 @@ class UserController extends Controller
 
     public function change_permission()
     {
-        $zam = Role::where('role', 'Заместитель')->first();
+        $zam = Role::where('name', 'Заместитель')->first();
         $zam->is_admin = !$zam->is_admin;
         $zam->save();
         return response()->json('',204);
