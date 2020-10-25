e75bd99aab788f987fec05f649f66c045e6d5382
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2020-10-25 11:29:04 +0600

    fx create

--- resources/views/checks/create.blade.php
+++ resources/views/checks/create.blade.php
@@ -1,141 +1,143 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="p-3 bg-form card-body-admin">
-        <div class="row">
-            <div class="col-12 col-sm-10 col-lg-12 col-md-10">
-                <form action="{{ route('inspector.store') }}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    <div class="row justify-content-center">
-                        <p class="font-weight-bold h2">Добавление проверки1</p>
-                    </div>
-                    <div class="form-group">
-                        <label class="font-weight-bold h6" for="type_check-select">Тип проверки:</label>
-                        <select class="form-control" name="type_id" id="type_check-select">
-                            @foreach($typeChecks as $typeCheck)
-                                <option value="{{ $typeCheck->id }}">{{ $typeCheck->name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="row ">
-                        <div class="col-lg-4 col-12 ">
-                            <div class="form-group d-flex">
-                                <div class="button r mr-3" id="button-1">
-                                    <input id="aups_check" type="checkbox" class="checkbox" name="has_aups">
-                                    <div class="knobs"></div>
-                                    <div class="layer"></div>
+    <div class="container">
+        <div class="p-3 bg-form card-body-admin">
+            <div class="row">
+                <div class="col-12 col-sm-10 col-lg-12 col-md-10">
+                    <form action="{{ route('inspector.store') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="row justify-content-center">
+                            <p class="font-weight-bold h2 text-center">Добавление проверки1</p>
+                        </div>
+                        <div class="form-group">
+                            <label class="font-weight-bold h6" for="type_check-select">Тип проверки:</label>
+                            <select class="form-control" name="type_id" id="type_check-select">
+                                @foreach($typeChecks as $typeCheck)
+                                    <option value="{{ $typeCheck->id }}">{{ $typeCheck->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="row ">
+                            <div class="col-lg-4 col-12 ">
+                                <div class="form-group d-flex">
+                                    <div class="button r mr-3" id="button-1">
+                                        <input id="aups_check" type="checkbox" class="checkbox" name="has_aups">
+                                        <div class="knobs"></div>
+                                        <div class="layer"></div>
+                                    </div>
+                                    <label class="font-weight-bold h5 pr-3" for="aups_check">АУПС</label>
                                 </div>
-                                <label class="font-weight-bold h5 pr-3" for="aups_check">АУПС</label>
+                            </div>
+                            <div class="col-lg-4 col-12 ">
+                                <div class="form-group d-flex">
+                                    <div class="button r mr-3" id="button-1">
+                                        <input id="aupt_check" type="checkbox" class="checkbox" name="has_aupt">
+                                        <div class="knobs"></div>
+                                        <div class="layer"></div>
+                                    </div>
+                                    <label class="font-weight-bold h5 pr-3" for="aupt_check">АУПТ</label>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-12">
+                                <div class="form-group d-flex">
+                                    <div class="button r mr-3" id="button-1">
+                                        <input id="has_cranes_check" type="checkbox" class="checkbox" name="has_cranes">
+                                        <div class="knobs"></div>
+                                        <div class="layer"></div>
+                                    </div>
+                                    <label class="font-weight-bold h5 pr-3" for="has_cranes_check">Кран</label>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-12">
+                                <div class="form-group d-flex">
+                                    <div class="button r mr-3" id="button-1">
+                                        <input id="has_evacuation_check" type="checkbox" class="checkbox"
+                                               name="has_evacuation">
+                                        <div class="knobs"></div>
+                                        <div class="layer"></div>
+                                    </div>
+                                    <label class="font-weight-bold h5 pr-3" for="has_evacuation_check">План
+                                        эвакуации</label>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-12">
+                                <div class="form-group d-flex">
+                                    <div class="button r mr-3" id="button-1">
+                                        <input id="has_hydrant_check" type="checkbox" class="checkbox" name="has_hydrant">
+                                        <div class="knobs"></div>
+                                        <div class="layer"></div>
+                                    </div>
+                                    <label class="font-weight-bold h5 pr-3" for="has_hydrant_check">Гидрант</label>
+                                </div>
+                            </div>
+                            <div class="col-lg-4 col-12">
+                                <div class="form-group d-flex">
+                                    <div class="button r mr-3" id="button-1">
+                                        <input id="has_reservoir_check" type="checkbox" class="checkbox"
+                                               name="has_reservoir">
+                                        <div class="knobs"></div>
+                                        <div class="layer"></div>
+                                    </div>
+                                    <label class="font-weight-bold h5 pr-3" for="has_reservoir_check">Водоем</label>
+                                </div>
+                            </div>
+                            <div class="col-lg-5 col-12">
+                                <div class="form-group d-flex">
+                                    <div class="button r mr-3" id="button-1">
+                                        <input id="has_foam_check" type="checkbox" class="checkbox" name="has_foam">
+                                        <div class="knobs"></div>
+                                        <div class="layer"></div>
+                                    </div>
+                                    <label class="font-weight-bold h5" for="has_foam_check">Запасы
+                                        пенооброзование</label>
+                                </div>
                             </div>
                         </div>
-                        <div class="col-lg-4 col-12 ">
-                            <div class="form-group d-flex">
-                                <div class="button r mr-3" id="button-1">
-                                    <input id="aupt_check" type="checkbox" class="checkbox" name="has_aupt">
-                                    <div class="knobs"></div>
-                                    <div class="layer"></div>
-                                </div>
-                                <label class="font-weight-bold h5 pr-3" for="aupt_check">АУПТ</label>
-                            </div>
+                        <div class="form-group py-2">
+                            <label class="font-weight-bold h6" for="image_input">Изображении</label>
+                            <input id="image_input" name="images[]" type="file" accept="image/*" multiple>
                         </div>
-                        <div class="col-lg-4 col-12">
-                            <div class="form-group d-flex">
-                                <div class="button r mr-3" id="button-1">
-                                    <input id="has_cranes_check" type="checkbox" class="checkbox" name="has_cranes">
-                                    <div class="knobs"></div>
-                                    <div class="layer"></div>
-                                </div>
-                                <label class="font-weight-bold h5 pr-3" for="has_cranes_check">Кран</label>
-                            </div>
-                        </div>
-                        <div class="col-lg-4 col-12">
-                            <div class="form-group d-flex">
-                                <div class="button r mr-3" id="button-1">
-                                    <input id="has_evacuation_check" type="checkbox" class="checkbox"
-                                           name="has_evacuation">
-                                    <div class="knobs"></div>
-                                    <div class="layer"></div>
-                                </div>
-                                <label class="font-weight-bold h5 pr-3" for="has_evacuation_check">План
-                                    эвакуации</label>
-                            </div>
-                        </div>
-                        <div class="col-lg-4 col-12">
-                            <div class="form-group d-flex">
-                                <div class="button r mr-3" id="button-1">
-                                    <input id="has_hydrant_check" type="checkbox" class="checkbox" name="has_hydrant">
-                                    <div class="knobs"></div>
-                                    <div class="layer"></div>
-                                </div>
-                                <label class="font-weight-bold h5 pr-3" for="has_hydrant_check">Гидрант</label>
-                            </div>
-                        </div>
-                        <div class="col-lg-4 col-12">
-                            <div class="form-group d-flex">
-                                <div class="button r mr-3" id="button-1">
-                                    <input id="has_reservoir_check" type="checkbox" class="checkbox"
-                                           name="has_reservoir">
-                                    <div class="knobs"></div>
-                                    <div class="layer"></div>
-                                </div>
-                                <label class="font-weight-bold h5 pr-3" for="has_reservoir_check">Водоем</label>
-                            </div>
-                        </div>
-                        <div class="col-lg-5 col-12">
-                            <div class="form-group d-flex">
-                                <div class="button r mr-3" id="button-1">
-                                    <input id="has_foam_check" type="checkbox" class="checkbox" name="has_foam">
-                                    <div class="knobs"></div>
-                                    <div class="layer"></div>
-                                </div>
-                                <label class="font-weight-bold h5" for="has_foam_check">Запасы
-                                    пенооброзование</label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group py-2">
-                        <label class="font-weight-bold h6" for="image_input">Изображении</label>
-                        <input id="image_input" name="images[]" type="file" accept="image/*" multiple>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <div class="form-group">
-                                <label class="font-weight-bold h6" for="type_psp_select">Первичные средства
-                                    пожаротушения:</label>
-                                <div id="psps_div">
-                                    {{--place for psps--}}
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    <label class="font-weight-bold h6" for="type_psp_select">Первичные средства
+                                        пожаротушения:</label>
+                                    <div id="psps_div">
+                                        {{--place for psps--}}
 
+                                    </div>
+                                    <button id="add_psp" class="btn btn-success mt-2" type="button">
+                                        <i class="fas fa-plus "><span class="px-4">Добавить ПСП</span></i>
+                                    </button>
                                 </div>
-                                <button id="add_psp" class="btn btn-success mt-2" type="button">
-                                    <i class="fas fa-plus "><span class="px-4">Добавить ПСП</span></i>
-                                </button>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="form-group">
+                                    <label class="font-weight-bold h6" for="type_psp_select">Нарушения:</label>
+                                    <div id="violations_div">
+                                        {{--place for violations--}}
+                                    </div>
+                                    <button id="add_violation" class="btn btn-success mt-2" type="button">
+                                        <i class="fas fa-plus"><span class="px-4">Добавить Нарушение</span></i>
+                                    </button>
+                                </div>
                             </div>
                         </div>
-                        <div class="col-lg-6">
-                            <div class="form-group">
-                                <label class="font-weight-bold h6" for="type_psp_select">Нарушения:</label>
-                                <div id="violations_div">
-                                    {{--place for violations--}}
-                                </div>
-                                <button id="add_violation" class="btn btn-success mt-2" type="button">
-                                    <i class="fas fa-plus"><span class="px-4">Добавить Нарушение</span></i>
-                                </button>
+                        <input type="hidden" name="build_id" value="{{ $id }}">
+                        <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
+                        <div class="row pt-3">
+                            <div class="col-12 text-center">
+                                <button type="submit" title="{{ __('Добавить проверку') }}"
+                                        class="btn n btn-success px-3 py-2">{{ __('Добавить проверку') }}</button>
                             </div>
                         </div>
-                    </div>
-                    <input type="hidden" name="build_id" value="{{ $id }}">
-                    <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
-                    <div class="row pt-3">
-                        <div class="col-12 text-center">
-                            <button type="submit" title="{{ __('Добавить проверку') }}"
-                                    class="btn n btn-success px-3 py-2">{{ __('Добавить проверку') }}</button>
-                        </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
 
+    </div>
 
 
 
@@ -225,7 +227,7 @@
         $('#add_psp').click(function () {
             let html =
                 `<div class="row card-body-admin my-2 mx-1 bg-form">
-        <div class="col-lg-4 d-flex align-items-center">
+        <div class="col-lg-5 d-flex align-items-center pt-2 pt-lg-0 justify-content-center">
             <select class="form-control-sm" name="type_psps[]" id="type_psp_select">`
                 @foreach($typePsps as $typePsp)
                 + `
@@ -233,7 +235,7 @@
                 @endforeach
                 + `</select>
         </div>
-        <div class="col-lg-7">
+        <div class="col-lg-6 text-center">
             <div class="py-2">
                 <input class="" name="counts[]" type="number" min="1" value="1" required style="padding: 1.25px 0;">
             </div>
@@ -254,7 +256,7 @@
             let html =
                 `
     <div class="row card-body-admin my-2 mx-1 bg-form"  style="padding:0.7px 0">
-        <div class="col-lg-5 d-flex align-items-center">
+        <div class="col-lg-5 d-flex align-items-center justify-content-center pt-2 pt-lg-0">
                 <select class="form-control-sm " name="type_violations[]" id="type_psp_select">`
                 @foreach($typeViolations as $typeViolation)
                 + `
@@ -262,7 +264,7 @@
                 @endforeach
                 + `</select>
         </div>
-        <div class="col-lg-6">
+        <div class="col-lg-6 text-center">
             <div style="padding-top: 6px">
                 <textarea class="" name="descs[]" cols=25" rows="1" style="padding: 2.25px 0;"></textarea>
             </div>
