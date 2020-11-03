c86561d2868297b9453d44b0d96f6b2bc5dc0441
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2020-11-03 17:53:45 +0600

    delete excess library datatables

--- resources/views/admin/types/index.blade.php
+++ resources/views/admin/types/index.blade.php
@@ -25,9 +25,6 @@
 
 @endsection
 
-@push('styles')
-    <link rel="stylesheet" href="//cdn.datatables.net/1.10.7/css/jquery.dataTables.min.css">
-@endpush
 
 @push('scripts')
     <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
