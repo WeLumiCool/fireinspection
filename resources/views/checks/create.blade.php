4b89a534fedea26496b2897a879f1dea1054513f
Azret <azret1jusuev@gmail.com>
2020-11-15 21:12:49 +0600

    fx bag

--- resources/views/checks/create.blade.php
+++ resources/views/checks/create.blade.php
@@ -71,7 +71,7 @@
 
                                     </div>
                                 </div>
-                            @endif
+                            @endforeach
                         </div>
                         <div class="row">
                             <div class="col-lg-12">
